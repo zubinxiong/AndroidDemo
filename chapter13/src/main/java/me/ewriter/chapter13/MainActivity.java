a7360ac32e0c44b5680f5add865e270179e4fd24
zubinxiong <zubinxiong@gmail.com>
2016-06-02 14:56:04 +0800

    puzzle and 2048 demo

--- chapter13/src/main/java/me/ewriter/chapter13/MainActivity.java
+++ chapter13/src/main/java/me/ewriter/chapter13/MainActivity.java
@@ -1,7 +1,12 @@
 package me.ewriter.chapter13;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import me.ewriter.chapter13.game2048.Game;
+import me.ewriter.chapter13.puzzle.XPuzzleActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void BtnXPuzzle(View view) {
+        startActivity(new Intent(this, XPuzzleActivity.class));
+    }
+
+    public void Btn2048(View view) {
+        startActivity(new Intent(this, Game.class));
     }
 }
