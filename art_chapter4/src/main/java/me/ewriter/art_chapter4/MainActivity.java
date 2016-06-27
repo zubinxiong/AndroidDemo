99e6b60ac4d04fecaef98a53c2298be8a293fdad
zubinxiong <zubinxiong@gmail.com>
2016-06-27 19:54:47 +0800

    update chapter4

--- art_chapter4/src/main/java/me/ewriter/art_chapter4/MainActivity.java
+++ art_chapter4/src/main/java/me/ewriter/art_chapter4/MainActivity.java
@@ -1,13 +1,65 @@
 package me.ewriter.art_chapter4;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onButtonClick(View view) {
+        if (view.getId() == R.id.button1) {
+            Intent intent = new Intent(MainActivity.this, TestActivity.class);
+            startActivity(intent);
+        } else if (view.getId() == R.id.button2) {
+
+        } else if (view.getId() == R.id.button3) {
+
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        Log.d(TAG, "onPause");
+        super.onPause();
+    }
+
+    @Override
+    protected void onStart() {
+        Log.d(TAG, "onStart");
+        super.onStart();
+    }
+
+    @Override
+    protected void onRestart() {
+        Log.d(TAG, "onRestart");
+        super.onRestart();
+    }
+
+    @Override
+    protected void onResume() {
+        Log.d(TAG, "onResume");
+        super.onResume();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.d(TAG, "onStop");
+        super.onStop();
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        Log.d(TAG, "onWindowFocusChanged, focus:" + hasFocus);
+        super.onWindowFocusChanged(hasFocus);
     }
 }
