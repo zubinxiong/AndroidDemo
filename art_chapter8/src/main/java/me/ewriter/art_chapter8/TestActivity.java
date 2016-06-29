5f43a3260dce749d685b4fd3f36baa0afc49df55
zubinxiong <zubinxiong@gmail.com>
2016-06-29 17:16:40 +0800

    init  art_chapter8

---
+++ art_chapter8/src/main/java/me/ewriter/art_chapter8/TestActivity.java
@@ -0,0 +1,85 @@
+package me.ewriter.art_chapter8;
+
+import android.content.Context;
+import android.graphics.PixelFormat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+
+public class TestActivity extends AppCompatActivity implements View.OnTouchListener {
+
+    private static final String TAG = "TestActivity";
+
+    private Button mCreateWindowButton;
+
+    private Button mFloatingButton;
+    private WindowManager.LayoutParams mLayoutParams;
+    private WindowManager mWindowManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+        initView();
+    }
+
+    private void initView() {
+        mCreateWindowButton = (Button) findViewById(R.id.button1);
+        mWindowManager = (WindowManager) getSystemService(Context.WINDOW_SERVICE);
+    }
+
+    public void onButtonClick(View view) {
+        if (view == mCreateWindowButton) {
+            mFloatingButton = new Button(this);
+            mFloatingButton.setText("click me");
+            mLayoutParams = new WindowManager.LayoutParams(
+                    WindowManager.LayoutParams.WRAP_CONTENT, WindowManager.LayoutParams.WRAP_CONTENT,
+                    0, 0, PixelFormat.TRANSPARENT);
+            mLayoutParams.flags = WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL
+                    | WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
+                    | WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED;
+            mLayoutParams.type = WindowManager.LayoutParams.TYPE_SYSTEM_ERROR;
+            mLayoutParams.gravity = Gravity.LEFT | Gravity.TOP;
+            mLayoutParams.x = 100;
+            mLayoutParams.y = 300;
+            mFloatingButton.setOnTouchListener(this);
+            mWindowManager.addView(mFloatingButton, mLayoutParams);
+        }
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        int rawX = (int) event.getRawX();
+        int rawY = (int) event.getRawY();
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                break;
+
+            case MotionEvent.ACTION_MOVE:
+                int x = (int) event.getX();
+                int y = (int) event.getY();
+                mLayoutParams.x = rawX;
+                mLayoutParams.y = rawY;
+                mWindowManager.updateViewLayout(mFloatingButton, mLayoutParams);
+                break;
+
+            case MotionEvent.ACTION_UP:
+                break;
+        }
+        return false;
+    }
+
+    @Override
+    protected void onDestroy() {
+        try {
+            mWindowManager.removeView(mFloatingButton);
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+        }
+        super.onDestroy();
+    }
+}
