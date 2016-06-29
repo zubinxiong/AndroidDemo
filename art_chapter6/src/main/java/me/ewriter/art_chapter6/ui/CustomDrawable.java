01e2a46c015739c260663dc9ecc2788c746a50eb
zubinxiong <zubinxiong@gmail.com>
2016-06-29 15:39:11 +0800

    art_chapter6 demo

---
+++ art_chapter6/src/main/java/me/ewriter/art_chapter6/ui/CustomDrawable.java
@@ -0,0 +1,46 @@
+package me.ewriter.art_chapter6.ui;
+
+import android.graphics.Canvas;
+import android.graphics.ColorFilter;
+import android.graphics.Paint;
+import android.graphics.PixelFormat;
+import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
+
+/**
+ * Created by Zubin on 2016/6/29.
+ */
+public class CustomDrawable extends Drawable {
+
+    private Paint mPaint;
+
+    public CustomDrawable(int color) {
+        mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mPaint.setColor(color);
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        final Rect r = getBounds();
+        float cx = r.exactCenterX();
+        float cy = r.exactCenterY();
+        canvas.drawCircle(cx, cy, Math.min(cx, cy), mPaint);
+    }
+
+    @Override
+    public void setAlpha(int alpha) {
+        mPaint.setAlpha(alpha);
+        invalidateSelf();
+    }
+
+    @Override
+    public void setColorFilter(ColorFilter colorFilter) {
+        mPaint.setColorFilter(colorFilter);
+        invalidateSelf();
+    }
+
+    @Override
+    public int getOpacity() {
+        return PixelFormat.TRANSLUCENT;
+    }
+}
